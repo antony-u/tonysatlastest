591afa770cea2f2947ed865f18299d6383ec5556
Antony Urbina Alvarado <antony.urbinaalvarado@wpengine.com>
2022-03-17 13:39:17 -0500

    headers test reversion

--- next.config.js
+++ next.config.js
@@ -3,21 +3,4 @@ const { withFaust } = require('@faustjs/next');
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = { withFaust,   async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'x-custom-header',
-            value: 'my custom header value',
-          },
-          {
-            key: 'x-another-custom-header',
-            value: 'my other custom header value',
-          },
-        ],
-      },
-    ]
-  },
-};
+module.exports = withFaust();
